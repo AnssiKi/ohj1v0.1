43ebe3e0fac358950b37a3bfad6c52dedfba7052
Ville Huupponen <ville.huupponen@gmail.com>
2024-04-18 12:01:50 +0300

    Luotu Asiakkaat sivun elementit ja tapahtumakäsittelijät

--- Asiakkaat.xaml.cs
+++ Asiakkaat.xaml.cs
@@ -6,4 +6,58 @@ public partial class Asiakkaat : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void alue_nimi_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void etunimi_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void sukunimi_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void lahiosoite_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void postinumero_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void email_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void tallenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tyhjenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void poista_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void hae_sukunimella_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void lista_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+
+    }
 }
