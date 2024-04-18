e2171629aac99628594800289e8fb0211addfa99
Ville Huupponen <ville.huupponen@gmail.com>
2024-04-18 10:15:23 +0300

    Luotu mökkisivun elementit ja tapahtumakäsittelijät

--- Mokit.xaml.cs
+++ Mokit.xaml.cs
@@ -6,4 +6,65 @@ public partial class Mokit : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void alue_nimi_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void mokki_nimi_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void mokki_katuosoite_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void mokki_postinumero_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void mokki_hinta_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void mokki_kuvaus_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void mokki_varustelu_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void mokki_henkilomaara_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void tallenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tyhjenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void poista_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void hae_nimella_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
 }
