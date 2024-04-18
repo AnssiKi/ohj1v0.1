c0a0fd500c0028c167e1dbc165d15438db71cb10
pimpieSankko <minna402@gmail.com>
2024-04-18 12:45:27 +0300

    Varaukset front

--- Varaukset.xaml.cs
+++ Varaukset.xaml.cs
@@ -6,4 +6,44 @@ public partial class Varaukset : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void alkupvm_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void loppupvm_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void vahvistuspvm_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void varauspvm_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void tallenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tyhjenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void poista_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void hae_varaukset_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
 }
