2116a9ed15fb514d352cd099a20132cf490b04d0
pimpieSankko <minna402@gmail.com>
2024-04-18 10:56:28 +0300

    Alueet front

--- Alueet.xaml.cs
+++ Alueet.xaml.cs
@@ -6,4 +6,29 @@ public partial class Alueet : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void alue_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void tallenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tyhjenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void poista_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void lista_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+
+    }
 }
