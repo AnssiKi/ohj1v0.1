b2ad0882f4a9fe2501671f5cb08561bee3d78c2f
Ville Huupponen <ville.huupponen@gmail.com>
2024-04-18 15:02:51 +0300

    Luotu TeeUusiVaraus sivun elementit ja tapahtumakäsittelijät

--- TeeUusiVaraus.xaml.cs
+++ TeeUusiVaraus.xaml.cs
@@ -6,4 +6,45 @@ public partial class TeeUusiVaraus : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void alue_nimi_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void alkupvm_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void loppupvm_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+
+    private void henkilomaara_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void mokki_lista_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+
+    }
+    private void palvelu_lkm_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void uusi_asiakas_Clicked(object sender, EventArgs e)
+    { // Tässä pelkästään navigointi uudelle sivulle 
+        // muut toiminnallisuudet puuttuvat vielä siirtyykö tieto mukana?
+        Navigation.PushAsync(new Uusi_asiakas(this));
+    }
+
+    private void vanha_asiakas_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
 }
