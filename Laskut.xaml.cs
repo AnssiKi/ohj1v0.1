60dfe22007aac715214bb21cf3e32ff903c87f9f
Ville Huupponen <ville.huupponen@gmail.com>
2024-04-18 12:23:53 +0300

    Luotu Laskusivun elementit ja tapahtumakäsittelijät

--- Laskut.xaml.cs
+++ Laskut.xaml.cs
@@ -6,4 +6,29 @@ public partial class Laskut : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void maksettu_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void tulosta_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tyhjenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void hae_paivamaaralla_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void lista_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+
+    }
 }
