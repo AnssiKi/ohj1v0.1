1d7c111143f30c13b42f5c55fb1eb60325c73d62
pimpieSankko <minna402@gmail.com>
2024-04-18 14:08:42 +0300

    Uusi asiakas, joka liitetään myöhemmin uusi varaus sivulle.

---
+++ Uusi_asiakas.xaml.cs
@@ -0,0 +1,50 @@
+namespace ohj1v0._1;
+
+public partial class Uusi_asiakas : ContentPage
+{
+	public Uusi_asiakas()
+	{
+		InitializeComponent();
+	}
+
+
+    private void alue_nimi_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void etunimi_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void sukunimi_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void lahiosoite_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void postinumero_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void puhelinnumero_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void email_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void tallenna_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
