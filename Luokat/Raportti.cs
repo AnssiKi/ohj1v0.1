44d9639faf862274b6c02ec4f4e9683ce2dcb595
Ville Huupponen <ville.huupponen@gmail.com>
2024-04-26 11:18:04 +0300

    Raportointi toimintoja aloitettu. Raportti - luokka luotu

---
+++ Luokat/Raportti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ohj1v0._1.Luokat
+{
+    internal class Raportti
+    {
+        public string Raporttityyppi { get; set; }
+        public string Alue { get; set; }
+        public DateTime Alkupvm { get; set; }
+        public DateTime Loppupvm { get; set; }
+        public double Yhteensa { get; set; }
+    }
+}
--- Pages/Raportit.xaml.cs
+++ Pages/Raportit.xaml.cs
@@ -1,16 +1,61 @@
 namespace ohj1v0._1;
+using ohj1v0._1.Luokat;
+using ohj1v0._1.Viewmodels;
+using ohj1v0._1.Models;
 
 public partial class Raportit : ContentPage
 {
-	public Raportit()
-	{
-		InitializeComponent();
-	}
-    private void hae_Clicked(object sender, EventArgs e)
+    public Raportit()
     {
-
+        InitializeComponent();
+        raportti = new List<Raportti>();
+        alue_nimi.BindingContext = alueViewmodel;
     }
 
+    AlueViewmodel alueViewmodel = new AlueViewmodel();
+    private List<Raportti> raportti;
+
+    private void hae_Clicked(object sender, EventArgs e)
+    {
+        string selectedRaportti = raportti_valinta.SelectedItem?.ToString();
+        Alue selectedAlue = (Alue)alue_nimi.SelectedItem;
+        DateTime startDate = alkupvm.Date;
+        DateTime endDate = loppupvm.Date;
+        string Raporttityyppi;
+
+        if (selectedRaportti == "Majoitus")
+        {
+            Raporttityyppi = "Majoitus";
+        }
+        else if (selectedRaportti == "Palvelut")
+        {
+            Raporttityyppi = "Palvelut";
+        }
+        else
+        {
+            return; // ei tehda mitaan jos raportin tyyppia ei ole valittu
+        }
+
+        if (selectedAlue == null)
+        {
+            return; // ei tehda mitaan jos raportin tyyppia ei ole valittu
+        }
+
+        Raportti newRaportti = new Raportti
+        {
+            Raporttityyppi = Raporttityyppi,
+            Alue = selectedAlue.Nimi,
+            Alkupvm = startDate,
+            Loppupvm = endDate
+            // Yhteensa = CRUD haku tietokannasta valituilla rajauksilla
+        };
+
+        raportti.Add(newRaportti);
+        lista.ItemsSource = null;
+        lista.ItemsSource = raportti;
+    }
+
+    
     private void alkupvm_DateSelected(object sender, DateChangedEventArgs e)
     {
 
